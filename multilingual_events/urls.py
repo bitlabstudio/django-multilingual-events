2b254b2ecf69e2844942a392dd5e62dd17752876
Martin Brochhaus <mbrochh@gmail.com>
2013-02-28 17:15:14 +0800

    Renamed view names

--- multilingual_events/urls.py
+++ multilingual_events/urls.py
@@ -1,19 +1,16 @@
 """URLs for the ``multilingual_events`` app."""
 from django.conf.urls.defaults import patterns, url
 
-from simple_events.views import (
-    EventDetailView,
-    EventListView,
-)
+from .views import EventDetailView, EventListView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$',
         EventListView.as_view(),
-        name='simple_events_list'),
+        name='multilingual_events_list'),
 
     url(r'^(?P<pk>\d+)/$',
         EventDetailView.as_view(),
-        name='simple_events_detail'),
+        name='multilingual_events_detail'),
 )
